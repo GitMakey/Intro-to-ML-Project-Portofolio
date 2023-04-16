6d70eb54d959e377aeaae3d4fdf94ee53365abad
Marina Epitropakis <>
2023-04-16 21:08:23 +0200

    added knn (method 2)

--- Project_2/ClassificationProject2.py
+++ Project_2/ClassificationProject2.py
@@ -2,15 +2,23 @@ import pandas as pd
 import numpy as np
 import os
 import math
-
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.linalg import svd
 from scipy.stats import zscore
 from scipy import stats
-
 from matplotlib.pylab import figure, plot, xlabel, ylabel, legend, ylim, show
 import sklearn.linear_model as lm
+from matplotlib.pyplot import (figure, plot, title, xlabel, ylabel, 
+                               colorbar, imshow, xticks, yticks, show)
+from scipy.io import loadmat
+from sklearn.neighbors import KNeighborsClassifier, DistanceMetric
+from sklearn.metrics import confusion_matrix
+from numpy import cov
+from sklearn.model_selection import train_test_split
+from sklearn import model_selection
+
+#Logistic Regression
 
 # Load the Real-Estate csv data using the Pandas library
 # URL = 'https://storage.googleapis.com/kagglesdsdata/datasets/898072/1523358/data.csv?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20230301%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20230301T160235Z&X-Goog-Expires=259200&X-Goog-SignedHeaders=host&X-Goog-Signature=1f4f05b433874186e8dea74d05881585e7bb23da6802a1a7d67bb61749fd784cfc845b318b1e35dd3e07a3d1fec99b1ad2b7f05d5eac6d584e9cfad6d5a6d2d414a259851464b32ad58db293bdb0c913a206a915a87155383f98ec1acb5f098aff7eb1a5f66ef10c1565caa6c8da14ed91c8f964235f216f595f6175cf279ed8e792e093f19b1aa4223d21bf404d6055a4f2b06a51af27513c10683662a56669d04d1fa0cbb5dc76b01f46c0af2ba27a7c31a018a3111c0eeb81dadeff2ebf87adc770e5bc7a28ca998c79d7c356b99cb3b93e2309c806b8ef6c8218a3290d33fddacfae95bf42308b0d00c22e1df9f1c54b94a80136f1aad8eec8d3e3459aeb'
@@ -98,5 +106,101 @@ ylim(-0.01,1.5)
 show()
 
 print('Logistic Regression')
+
+# Method 2: KNN
+
+# Load Matlab data file and extract variables of interest
+X_train, X_test = train_test_split(X, test_size=0.2, random_state=42)
+y_train, y_test = train_test_split(y, test_size=0.2, random_state=42)
+C = 3
+
+# Plot the training data points (color-coded) and test data points.
+figure(1)
+styles = ['.b', '.r', '.g', '.y']
+for c in range(C):
+    class_mask = (y_train==c)
+    plot(X_train[class_mask,0], X_train[class_mask,1], styles[c])
+
+
+# K-nearest neighbors
+K=5
+
+# Distance metric (corresponds to 2nd norm, euclidean distance).
+# You can set dist=1 to obtain manhattan distance (cityblock distance).
+dist=2
+metric = 'minkowski'
+metric_params = {} # no parameters needed for minkowski
+
+# You can set the metric argument to 'cosine' to determine the cosine distance
+#metric = 'cosine' 
+#metric_params = {} # no parameters needed for cosine
+
+# To use a mahalonobis distance, we need to input the covariance matrix, too:
+#metric='mahalanobis'
+#metric_params={'V': cov(X_train, rowvar=False)}
+
+# Fit classifier and classify the test points
+knclassifier = KNeighborsClassifier(n_neighbors=K, p=dist, 
+                                    metric=metric,
+                                    metric_params=metric_params)
+knclassifier.fit(X_train, y_train)
+y_est = knclassifier.predict(X_test)
+
+
+# Plot the classfication results
+styles = ['ob', 'or', 'og', 'oy']
+for c in range(C):
+    class_mask = (y_est==c)
+    plot(X_test[class_mask,0], X_test[class_mask,1], styles[c], markersize=10)
+    plot(X_test[class_mask,0], X_test[class_mask,1], 'kx', markersize=8)
+title('Synthetic data classification - KNN');
+
+# Compute and plot confusion matrix
+cm = confusion_matrix(y_test, y_est);
+accuracy = 100*cm.diagonal().sum()/cm.sum(); error_rate = 100-accuracy;
+figure(2);
+imshow(cm, cmap='binary', interpolation='None');
+colorbar()
+xticks(range(C)); yticks(range(C));
+xlabel('Predicted class'); ylabel('Actual class');
+title('Confusion matrix (Accuracy: {0}%, Error Rate: {1}%)'.format(accuracy, error_rate));
+
+show()
+
+print('KNN Classification')
+
+
+# Maximum number of neighbors
+L=40
+
+CV = model_selection.LeaveOneOut()
+errors = np.zeros((N,L))
+i=0
+for train_index, test_index in CV.split(X, y):
+    print('Crossvalidation fold: {0}/{1}'.format(i+1,N))    
+    
+    # extract training and test set for current CV fold
+    X_train = X[train_index,:]
+    y_train = y[train_index]
+    X_test = X[test_index,:]
+    y_test = y[test_index]
+
+    # Fit classifier and classify the test points (consider 1 to 40 neighbors)
+    for l in range(1,L+1):
+        knclassifier = KNeighborsClassifier(n_neighbors=l);
+        knclassifier.fit(X_train, y_train);
+        y_est = knclassifier.predict(X_test);
+        errors[i,l-1] = np.sum(y_est[0]!=y_test[0])
+
+    i+=1
+    
+# Plot the classification error rate
+figure()
+plot(100*sum(errors,0)/N)
+xlabel('Number of neighbors')
+ylabel('Classification error rate (%)')
+show()
+
+print('Ran Leave one out cross validation')
 
 
